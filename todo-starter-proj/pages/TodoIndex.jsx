7e5a9a60e4ebfc029080e5cd3f524a3aafb130e7
Denis Libin <dnslibin4@gmail.com>
2024-07-04 17:58:16 +0300

    Initial commit

---
+++ todo-starter-proj/pages/TodoIndex.jsx
@@ -0,0 +1,77 @@
+const { useState, useEffect } = React
+const { Link, useSearchParams } = ReactRouterDOM
+const { useSelector, useDispatch } = ReactRedux
+
+import { TodoFilter } from "../cmps/TodoFilter.jsx"
+import { TodoList } from "../cmps/TodoList.jsx"
+import { DataTable } from "../cmps/data-table/DataTable.jsx"
+import { todoService } from "../services/todo.service.js"
+import { loadTodos, removeTodo, saveTodo } from '../../store/actions/todo.actions.js'
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
+
+export function TodoIndex() {
+    // const [todos, setTodos] = useState(null)
+    const todos = useSelector(storeState => storeState.todos)
+    const isLoading = useSelector(storeState => storeState.isLoading)
+    const dispatch = useDispatch()
+
+    // Special hook for accessing search-params:
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const defaultFilter = todoService.getFilterFromSearchParams(searchParams)
+
+    const [filterBy, setFilterBy] = useState(defaultFilter)
+
+    useEffect(() => {
+        setSearchParams(filterBy)
+        loadTodos(filterBy)
+            .catch(err => {
+                console.log('err', err)
+                showErrorMsg('Cannot load todos')
+            })
+    }, [filterBy])
+
+    function onRemoveTodo(todoId) {
+        removeTodo(todoId)
+            .then(() => showSuccessMsg('Todo removed'))
+            .catch(err => {
+                console.log('err', err)
+                showErrorMsg('Cannot remove todo')
+            })
+    }
+
+    function onToggleTodo(todo) {
+        const todoToSave = { ...todo, isDone: !todo.isDone }
+        todoService.save(todoToSave)
+            .then((savedTodo) => {
+                setTodos(prevTodos => prevTodos.map(currTodo => (currTodo._id !== todo._id) ? currTodo : { ...savedTodo }))
+                showSuccessMsg(`Todo is ${(savedTodo.isDone) ? 'done' : 'back on your list'}`)
+            })
+            .catch(err => {
+                console.log('err:', err)
+                showErrorMsg('Cannot toggle todo ' + todoId)
+            })
+    }
+
+    // if (!todos) return <div>Loading...</div>
+    return (
+        <section className="todo-index">
+            <TodoFilter filterBy={filterBy} onSetFilterBy={setFilterBy} />
+            <div>
+                <Link to="/todo/edit" className="btn" >Add Todo</Link>
+            </div>
+            <h2>Todos List</h2>
+            {isLoading
+                ? <div>Loading...</div>
+                : <TodoList
+                    todos={todos}
+                    onRemoveTodo={onRemoveTodo}
+                    onToggleTodo={onToggleTodo} />}
+            <hr />
+            <h2>Todos Table</h2>
+            <div style={{ width: '60%', margin: 'auto' }}>
+                <DataTable todos={todos} onRemoveTodo={onRemoveTodo} />
+            </div>
+        </section>
+    )
+}
